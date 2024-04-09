837efb59def91d7876bbfb5fd94aa815664c0d0d
Klevis <klevi.dervishi@gmail.com>
2024-04-09 22:31:29 +0300

    Creating the signin page and connecting it with the database

--- src/components/LogIn/login.jsx
+++ src/components/LogIn/login.jsx
@@ -1,46 +1,68 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Cookies from 'js-cookie'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
 import './login.css'
 //login function
 function Login(){
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
+    const [error, setError] = useState('')
+    const [IPAddress, setIPAddress] = useState('')
     const navigate= useNavigate()
+    useEffect(() => {
+        fetch('https://api.ipify.org?format=json')
+          .then(response => response.json())
+          .then(data =>{
+             setIPAddress(data.ip)
+            })
+          .catch(error => console.log(error))
+      }, []);
     function handleSubmit(e){
         e.preventDefault();
-        Cookies.set('user','Hello')
-        navigate('/home')
+        if(username, password != ''){
+                fetch('http://localhost:5000/signin', {mode: 'cors'})
+                .then((res) => {
+                    res.json()
+                }).then((res)=> {
+                    console.log(IPAddress)
+                    Cookies.set('user','Hello')
+                    navigate('/home')
+                })
+        }else{
+            setError(<Alert severity="warning">Please fill all the fields!</Alert>)
+        }
     }
     return(
         <>
             <div className='logIn'>
                 <div className='form-box' >
                     <div className='form'>
-                        <h1 className='login-title'>Log In</h1>
+                        <h2 className='login-title'>Log In</h2>
+                        {error} 
                         <TextField 
                         id="username" 
                         label="Username" 
                         variant="standard"
+                        required
                         onChange={(e)=> setUsername(e.target.value)}
                         sx={{
                             marginBottom: 2,
                         }}
                         />
-                        <br />
                         <TextField 
                         id="password" 
                         label="Password" 
                         type='password'
                         variant="standard"
+                        required
                         onChange={(e)=> setPassword(e.target.value)}
                         sx={{
                             marginBottom: 2,
                         }}
                         />
-                        <br />
                         <Button
                          variant="outlined"
                          onClick={handleSubmit}
@@ -50,6 +72,7 @@ function Login(){
                             marginTop: 2,
                             
                         }}>Submit</Button>
+                        <p>If you don't have an accont <Link to={'/signup'}>create one</Link></p>
                     </div>
                 </div>
             </div>
--- src/components/SignIn/signIn.jsx
+++ src/components/SignIn/signIn.jsx
@@ -1,6 +1,149 @@
+import {useState, useEffect} from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import bcrypt from 'bcryptjs'
+import './signin.css';
 function Signin(){
+    //setting state variables
+    const [name, setName] = useState('')    
+    const [lastname, setLastname] = useState('')
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState('')
+    const [IPAddress, setIPAddress] = useState('')
+    //initializing useNavigate
+    const navigate= useNavigate()
+    //fetch to find the api of the device
+    useEffect(() => {
+        fetch('https://api.ipify.org?format=json')
+          .then(response => response.json())
+          .then(data =>{
+            setIPAddress(String(data.ip))
+            })
+          .catch(error => console.log(error))
+      }, []);
+    //envent handle for submiting the data of the signin form
+    function  handleSubmit(e){
+        e.preventDefault();
+        if(name, lastname,username, password, email != ''){
+            //validating the email
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)){
+                setError(<Alert severity="warning">Please enter a valid email!</Alert>)
+            }else{
+                //hashing sensitive data
+                const id = bcrypt.hashSync(username, parseInt(import.meta.env.VITE_SALT))
+                const passwordHash = bcrypt.hashSync(password, parseInt(import.meta.env.VITE_SALT))
+                //fetch the data to the backend to save them 
+                fetch(import.meta.env.VITE_SIGNIN, {mode: 'cors', method:"POST", headers:{'Content-Type':'application/json'}, body: JSON.stringify({
+                    "_id": id,
+                    "name": name,
+                    "lastname": lastname,
+                    "email": email,
+                    "username": username,
+                    "password": passwordHash,
+                    "ip": [
+                        {
+                            "ip_adress": IPAddress
+                        }
+                    ],
+                    "messages": []
+                })})
+                .then((res) => {
+                    if(res.status === 200){
+                        if (res.status  !== 404){
+                            console.log(res)
+                            Cookies.set('user', 'hello')
+                            navigate('/home')
+                        }else{
+                            console.log('problem with the server')
+                        }
+                    }else if(res.status === 401){
+                        setError(<Alert severity="warning">Username already exists!</Alert>)
+                    }
+                }).catch(error=>{
+                    console.log(error)
+                })
+            }
+        }else{
+            setError(<Alert severity="warning">Please fill all your fields!</Alert>)
+        }
+    }
     return(
-        <h2>signup</h2>
+        <>
+            <div className='logIn'>
+                <div className='form-box' >
+                    <div className='form'>
+                        <h2 className='login-title'>SignUp</h2>
+                        {error}
+                        <TextField 
+                        id="name-signup" 
+                        label="Name" 
+                        variant="standard"
+                        required
+                        onChange={(e)=> setName(e.target.value)}
+                        sx={{
+                            marginBottom: 2,
+                        }}
+                        />
+                        <TextField 
+                        id="lastname-signup" 
+                        label="Lastname" 
+                        variant="standard"
+                        required
+                        onChange={(e)=> setLastname(e.target.value)}
+                        sx={{
+                            marginBottom: 2,
+                        }}
+                        /> 
+                        <TextField 
+                        id="username-signup" 
+                        label="Username" 
+                        variant="standard"
+                        required
+                        onChange={(e)=> setUsername(e.target.value)}
+                        sx={{
+                            marginBottom: 2,
+                        }}
+                        />
+                        <TextField 
+                        id="email-signup" 
+                        label="Email" 
+                        variant="standard"
+                        type='email'
+                        required
+                        onChange={(e)=> setEmail(e.target.value)}
+                        sx={{
+                            marginBottom: 2,
+                        }}
+                        />
+                        <TextField 
+                        id="password-signup" 
+                        label="Password" 
+                        type='password'
+                        variant="standard"
+                        required
+                        onChange={(e)=> setPassword(e.target.value)}
+                        sx={{
+                            marginBottom: 2,
+                        }}
+                        />
+                        <Button
+                         variant="outlined"
+                         onClick={handleSubmit}
+                         id='submit-button-signUp'
+                         type='submit'
+                         sx={{
+                            marginTop: 2,
+                            
+                        }}>Submit</Button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 export default Signin
